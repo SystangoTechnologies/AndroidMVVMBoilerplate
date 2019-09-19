e9fa2c750800ccd7926fa4bab321c68548589375
Mohit Rajput <mrajput@isystango.com>
2019-09-19 18:46:55 +0530

    initial: Basic project setup with multiple environments

---
+++ app/src/main/java/com/systango/mvvm/MvvmApplication.kt
@@ -0,0 +1,12 @@
+package com.systango.mvvm
+
+import android.app.Application
+
+/**
+ * Created by Mohit Rajput on 19/9/19.
+ */
+class MvvmApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
